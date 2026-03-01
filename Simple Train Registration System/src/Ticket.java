268d5b683ec0496e05b83721b93a618b004e5cac
Aya Safwat <yukajk20019@gmail.com>
2026-03-01 23:25:03 +0200

    Add Train Registration System Files

---
+++ Simple Train Registration System/src/Ticket.java
@@ -0,0 +1,13 @@
+
+public class Ticket {
+    int id;
+    String route;
+    int seats;
+    String date;
+    String time;
+    String seatClass;
+
+    public void getInfo(int id, String route, char seatClass, int seats, String date, String time){
+
+    }
+}
